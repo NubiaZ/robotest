88f954f499b12116fe81ba0c04aa3257d56bdbf5
nubiaZ <nubiaxz@icloud.com>
2025-01-30 17:57:48 -0600

    added and changed stuff

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
-public class armSubsystem extends SubsystemBase{
+public class IntakeSubsystem extends SubsystemBase{
     
 }
